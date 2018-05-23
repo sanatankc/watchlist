61424f0b384894e88032c09431e05d590c5404eb
sanatankumar <sanatan.iit@gmail.com>
2018-05-23 05:39:17 +0530

    removed console

--- src/db_queries/getAddedMoviesByUser.js
+++ src/db_queries/getAddedMoviesByUser.js
@@ -5,6 +5,5 @@ module.exports = async (username) => {
   const userMovie =  await UserMovie.find({ username })
   const filteredForTmdbId = userMovie.map(data => ({tmdbId: data.tmdbId}))
   const movies = await Movie.find({$or: filteredForTmdbId})
-  console.log(movies)
   return movies
 }
